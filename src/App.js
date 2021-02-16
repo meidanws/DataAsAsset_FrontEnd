692700436bb85215f6581dc1f7a9d412de28aa46
Weiss Meidan <E011102@esl.corp.elbit.co.il>
2021-02-16 17:50:20 +0200

    Meidan fix meta data

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import GlobalStyles from 'src/components/GlobalStyles';
 import 'src/mixins/chartjs';
 import theme from 'src/theme';
 import routes from 'src/routes';
+import MetaTags from 'react-meta-tags';
 
 const App = () => {
   const routing = useRoutes(routes);
 
   return (
+ 
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <MetaTags>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+     </MetaTags>
       {routing}
     </ThemeProvider>
   );
